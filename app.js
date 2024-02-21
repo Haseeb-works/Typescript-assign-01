aff4534f0e1ce52428512da87ee963e03cf1217f
Haseeb <haseeb200421@gmail.com>
2024-02-21 11:20:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("haseeb ali");
+console.log("Hello wrold Project 001");
+console.log("Thank you Governer Sindh and all the team for teaching me");
---
+++ app.ts
@@ -0,0 +1,3 @@
+console.log("haseeb ali")
+console.log("Hello wrold Project 001")
+console.log("Thank you Governer Sindh and all the team for teaching me")
